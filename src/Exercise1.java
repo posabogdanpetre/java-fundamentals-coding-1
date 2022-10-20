1e571e7bf435edb29c84f5e879a386625a3fcc94
Posa Bogdan <bogdan.posa@kambi.com>
2022-10-20 19:59:09 +0300

    commit 4

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -5,11 +5,7 @@ public class Exercise1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         float diameter = scanner.nextFloat();
-        System.out.println(diameter * Math.PI);
-        System.out.println(diameter * Math.PI);
-        System.out.println(diameter * Math.PI);
-        System.out.println(diameter * Math.PI);
-        System.out.println(diameter * Math.PI);
+        System.out.println("GIT ESTE TARE!!!!");
 
     }
 }
